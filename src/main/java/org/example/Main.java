35b70427593f150ac7f1602508a344094f93558a
Hüseyin Yetişir <69421940+Huseyinyetisir145@users.noreply.github.com>
2023-03-04 16:57:08 +0300

    initial commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,18 @@
-package org.example;public class Main {
+package org.example;
+
+import org.example.solution.ListNode;
+import org.example.solution.MergeTwoSortedLists;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+//        List<Integer> list1 = Arrays.asList(1,2,4);
+//        List<Integer> list2 = Arrays.asList(1,2,3);
+//
+//        MergeTwoSortedLists test = new MergeTwoSortedLists();
+//        ListNode mergedList = test.mergeTwoSortedList(list1, list2);
+
     }
 }
