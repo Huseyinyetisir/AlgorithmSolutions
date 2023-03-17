777242685b27ac87dfba829a94d34d04be5f04d3
Hüseyin Yetişir <69421940+Huseyinyetisir145@users.noreply.github.com>
2023-03-17 10:02:47 +0300

    New Solutions

--- src/main/java/org/example/solution/ReturnDuplicates.java
+++ src/main/java/org/example/solution/ReturnDuplicates.java
@@ -1,6 +1,8 @@
 package org.example.solution;
 
 
+import java.util.Arrays;
+
 public class ReturnDuplicates {
 
     public static String returnDuplicates(String my_input) {
@@ -8,10 +10,10 @@ public class ReturnDuplicates {
         String duplicates = "";
         for (int i = 0; i < my_input.length(); i++) {
             if (duplicates.indexOf(my_input.charAt(i)) < 0) {
-                System.out.println("IndexOf : "+duplicates.indexOf(my_input.charAt(i)));
+                //System.out.println("IndexOf : "+duplicates.indexOf(my_input.charAt(i)));
                 for (int j = i + 1; j < my_input.length() - 1; j++) {
                     if (my_input.charAt(i) == my_input.charAt(j)) {
-                        System.out.println(my_input.charAt(i) +" == "+my_input.charAt(j));
+                        //      System.out.println(my_input.charAt(i) +" == "+my_input.charAt(j));
                         duplicates += my_input.charAt(i);
                         break;
                     }
@@ -21,7 +23,16 @@ public class ReturnDuplicates {
         }
         return duplicates;
     }
+
+
+
+    public static int returnDuplicateInt (int x){
+
+        return x;
+
+    }
     public static void main(String[] args) {
-        System.out.println(returnDuplicates("Hellllooo"));
+//        System.out.println(returnDuplicates("Hellllooo"));
+        System.out.println(returnDuplicateInt(5));
     }
 }
